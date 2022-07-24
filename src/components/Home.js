d0a25ce97fc28527ddad41dee6164c9bc8434152
Sameep Hedaoo <sameephedaoo123@gmail.com>
2022-07-24 20:25:53 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,34 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section />
+            <Section
+                title="Model S"
+                desc="Order Online for Touchless Delivery"
+                backImg="model-S.jpg"
+                leftBtn="Custom Order"
+                rightBtn="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                desc="Order Online for Touchless Delivery"
+                backImg="model-y.jpg"
+                leftBtn="Custom Order"
+                rightBtn="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                desc="Order Online for Touchless Delivery"
+                backImg="model-3.jpg"
+                leftBtn="Custom Order"
+                rightBtn="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                desc="Order Online for Touchless Delivery"
+                backImg="model-x.jpg"
+                leftBtn="Custom Order"
+                rightBtn="Existing Inventory"
+            />
         </Container>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
-function Section() {
+function Section(props) {
     return (
-        <Wrap>
+        <Wrap bgImage={props.backImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for touchless delivery</p>
+                <h1>{props.title}</h1>
+                <p>{props.desc}</p>
             </ItemText>
             <Buttons>
                 <ButtonGroup>
                     <LeftButton>
-                        Custom Order
+                        {props.leftBtn}
                     </LeftButton>
                     <RightButton>
-                        Existing Inventory
+                        {props.rightBtn}
                     </RightButton>
                 </ButtonGroup>
                 <DownArrow src="/images/down-arrow.svg" />
@@ -31,21 +31,26 @@ const Wrap = styled.div`
     background-size:cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image:url('/images/model-s.jpg');
+    // background-image:url('/images/model-s.jpg');
     display: flex;
     flex-direction:column;
     justify-content:space-between;
     align-items:center;
+    background-image: ${props => `url("/images/${props.bgImage}")`};
 `
 const ItemText = styled.div`
-    padding-top:15vh;
+    padding-top:15vh;   
     text-align: center;
 `
 const ButtonGroup = styled.div`
     text-align: center;
     display:flex;
-    margin-bottom:30px;
+    margin-bottom:10px;
     cursor:pointer;
+    @media (max-width: 768px)
+    {
+        flex-direction: column;
+    }
 `
 const LeftButton = styled.div`
     height:40px;
@@ -59,17 +64,24 @@ const LeftButton = styled.div`
     opacity:0.85;
     text-transform:uppercase;
     font-size: 12px;
+    margin: 10px;
 `
 const RightButton = styled(LeftButton)`
-    
+    background: white;
+    opacity: 0.65;
+    color: black;
 `
 const DownArrow = styled.img`
     margin-top: 20px;
+    margin-bottom:15px;
     height: 40px;
-    
+    animation: animateDown infinite 1.5s
+    // overflow-x: hidden;
     
 `
 const Buttons = styled.div`
     display:flex;
     flex-direction:column;
+    
 `
+
